a4b59a824a6310d2205e5bd07f5d71d6fff9bacd
guilhermedelgado18 <guilhermedelgado18@gmail.com>
2025-06-02 02:24:57 -0300

    corrigi o problema de adicionar vídeos quando não há usuários

--- cms/templates/Videos/add.php
+++ cms/templates/Videos/add.php
@@ -1,9 +1,16 @@
 <h1>Adicionar Vídeo</h1>
-<?= $this->Form->create($video) ?>
-<?= $this->Form->control('titulo') ?>
-<?= $this->Form->control('descricao') ?>
-<?= $this->Form->control('url') ?>
-<?= $this->Form->control('user_id', ['options' => $users]) ?>
-<?= $this->Form->button('Salvar') ?>
-<?= $this->Form->end() ?>
-<?= $this->Html->link('Voltar', ['controller' => 'Videos', 'action' => 'index'], ['class' => 'button']) ?>
+
+<?php if ($users->isEmpty()) : ?>
+    <p style="color:red;">⚠️ Não há usuários cadastrados. Cadastre um usuário antes de adicionar vídeos.</p>
+    <?= $this->Html->link('Ir para Usuários', ['controller' => 'Users', 'action' => 'add'], ['class' => 'button']) ?>
+<?php else : ?>
+    <?= $this->Form->create($video) ?>
+    <?= $this->Form->control('titulo') ?>
+    <?= $this->Form->control('descricao') ?>
+    <?= $this->Form->control('url') ?>
+    <?= $this->Form->control('user_id', ['options' => $users]) ?>
+    <?= $this->Form->button('Salvar') ?>
+    <?= $this->Form->end() ?>
+<?php endif; ?>
+
+<?= $this->Html->link('Voltar à Home', ['controller' => 'Home', 'action' => 'index'], ['class' => 'button']) ?>
